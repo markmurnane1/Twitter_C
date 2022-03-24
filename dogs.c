72190cbd1893005a582a346da34cbbcb4f0e5a17
ebrighton <elliot.brighton@ucdconnect.ie>
2022-03-24 17:33:30 +0000

    modified dog file

--- dogs.c
+++ dogs.c
@@ -3,7 +3,7 @@
 int main()
 {
 
-    printf("puppys \n canine \n wolf \n sdhfgewhgfjwg");
+    printf("puppys \n canine \n wolf \n sdhfgewhgfjwg\n bow wow\n pikachu\n charmander\n squirtle\n bulbasaur\n ");
 
 
     return 0;
