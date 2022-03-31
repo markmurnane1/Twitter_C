2aa0d3ed7438b0c1dfed90652d76180927e27385
ebrighton <elliot.brighton@ucdconnect.ie>
2022-03-31 16:23:22 +0100

    basic files added

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "twitter_create.h"
+
+
+int main() {
+
+    twitter twitter_system;
+    create_twitter_system(&twitter_system);
+
+    //implement here the code to print the users
+    // for each user you need to print the username, the number of followers and the number of users that the current user is following
+    
+
+
+
+}
---
+++ twitter_create.h
@@ -0,0 +1,49 @@
+//
+// Created by Lili on 30/03/2022.
+//
+
+#ifndef ASSIGNMENT2_TWITTER_CREATE_H
+#define ASSIGNMENT2_TWITTER_CREATE_H
+
+#endif //ASSIGNMENT2_TWITTER_CREATE_H
+
+//Max length of a tweet
+#define TWEET_LENGTH 270
+
+//Max length of a username
+#define USR_LENGHT 100
+
+//Max num of a followers
+#define MAX_FOLLOWERS 30
+
+//Max num of following users
+#define MAX_FOLLOWING 30
+
+//Max num of tweets in the news feed
+#define MAX_TWEETS 100
+
+//Max num of users
+#define MAX_USERS 100
+
+typedef struct tweet{
+    int id;
+    char msg[TWEET_LENGTH];
+    char user[USR_LENGHT];
+
+}tweet;
+
+typedef struct user{
+    char username[USR_LENGHT];
+
+    char followers[MAX_FOLLOWERS];
+    int num_followers;
+
+    char following[MAX_FOLLOWING];
+    int num_following;
+}user;
+
+typedef struct twitter{
+   // to be completed
+} twitter;
+
+void create_twitter_system(twitter * twitter_system);
