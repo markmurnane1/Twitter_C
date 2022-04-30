1e454e09cde605854a405649e8805a5d2af37fed
ebrighton <elliot.brighton@ucdconnect.ie>
2022-04-30 13:52:34 +0100

    updated function prototypes

--- twitter_functionality.h
+++ twitter_functionality.h
@@ -8,11 +8,15 @@
 #endif //COMP10050_TWITTER_FUNCTIONALITY_H
 
 void menu(twitter * twitter_system, int x);
-void getNewsFeed(twitter * twitter_system, int x);
-void postTweet(twitter * twitter_system, int x);
+
+void getNewsFeed(twitter * twitter_system, int x, tweetPtr currentPtr);
+void postTweet(twitter * twitter_system, int x, tweetPtr *sPtr);
+
 void follow(twitter * twitter_system, int x);
 void unfollow(twitter * twitter_system, int x);
-void delete(twitter * twitter_system, int x);
 
+int delete(tweetPtr *sPtr, twitter * twitter_system, int x);
 int endTurn(twitter * twitter_system, int x);
+
+void instructions(void);
 void endTwitter(void);
